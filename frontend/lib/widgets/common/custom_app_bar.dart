63700167b480a4cf809ef10b84e5d7f66e540bf7
Sanjeewa <san.palli@icloud.com>
2026-01-29 09:00:00 +0530

    Create reusable CustomAppBar widget with title and action support

---
+++ frontend/lib/widgets/common/custom_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../config/app_theme.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? actions;
+  final bool showBack;
+  final Widget? leading;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.actions,
+    this.showBack = true,
+    this.leading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: AppTheme.backgroundColor,
+      elevation: 0,
+      centerTitle: true,
+      leading: leading ??
+          (showBack && Navigator.canPop(context)
+              ? IconButton(
+                  icon: const Icon(Icons.arrow_back_ios_new, size: 20),
+                  onPressed: () => Navigator.pop(context),
+                )
+              : null),
+      title: Text(
+        title,
+        style: GoogleFonts.poppins(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          color: AppTheme.fontColor,
+        ),
+      ),
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
